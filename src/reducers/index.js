612f822fd1dac98178b18215ac7223fa5cae2c5a
abanoub nagib <abanoub.nagib@hotmail.com>
2023-08-31 05:03:48 -0700

    completed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,12 @@
 import { combineReducers } from 'redux';
 
+import favReducer from './favoritesReducer';
+
 import movieReducer from './movieReducer';
 
-export default movieReducer;
+const rootReducer = combineReducers({
+    movieReducer,
+    favReducer
+})
+
+export default rootReducer;
